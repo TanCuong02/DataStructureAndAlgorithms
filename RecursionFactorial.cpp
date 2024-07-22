c6c8f6a1d63f8386480bc453e4e630c7ea024b74
TanCuong02 <106979714+TanCuong02@users.noreply.github.com>
2024-07-22 13:20:53 +0700

    update

--- RecursionFactorial.cpp
+++ RecursionFactorial.cpp
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 int giaithua(int n){
-	if(n==0 || n==1){
-    	return 1;
-    }else{
-    	return n*giaithua(n-1);
-    }
-
+	if (n > 0) {
+      return n * giaithua(n - 1);
+   }
+   else {
+      return 1;
+   }
 }
 
 int main() {
